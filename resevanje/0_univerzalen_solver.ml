3c5ce96e3d8b39a54d0f65fc357dbfc572b6373d
Docker <docker@example.com>
2022-12-01 14:46:52 +0000

    univerzalna datoteka za resevanje

---
+++ resevanje/0_univerzalen_solver.ml
@@ -0,0 +1,30 @@
+let dan = ""
+
+let preberi_datoteko ime_datoteke =
+  let chan = open_in ime_datoteke in
+  let vsebina = really_input_string chan (in_channel_length chan) in
+  close_in chan;
+  vsebina
+
+let izpisi_datoteko ime_datoteke vsebina =
+  let chan = open_out ime_datoteke in
+  output_string chan vsebina;
+  close_out chan
+
+let vsebina_datoteke = preberi_datoteko ("resevanje/"^dan^"dan/input.in")
+
+let naloga1 vsebina =
+  "TODO"
+
+let naloga2 vsebina =
+  "TODO"
+
+let odgovor1 = naloga1 vsebina_datoteke
+let odgovor2 = naloga2 vsebina_datoteke
+
+(*
+let _ = izpisi_datoteko ("resevanje/"^dan^"dan/odgovor1.out") odgovor1
+let _ = izpisi_datoteko ("resevanje/"^dan^"dan/odgovor2.out") odgovor2
+*)
+
+(* če je odgovor v tipu int, popravi odgovor1 -> (Int.to_string odgovor1) *)
